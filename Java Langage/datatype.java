f472bfbc643579af181092bfcc2db9a7ff30269c
Rahul Kumar <rahulkumar906960@gmail.com>
2026-05-31 16:04:29 +0530

    Add files via upload

---
+++ Java Langage/datatype.java
@@ -0,0 +1,30 @@
+/*------------------Datatype---------------------------*/
+
+class datatype{
+    public static void main(String[] args){
+        byte x = 5;
+        int a = 48;
+        float b = 6.9f;
+        int c = x + a;
+        float s = 6.54f + x;
+        System.out.println(c);
+        System.out.println(s);
+    }
+}
+
+/*-------------Increment And Decrement----------------------------*/
+
+class datatype{
+    public static void main(String[] args){
+        int i = 54;
+        int a = i++;//First i is use and then Increment
+        int c = ++i;
+        int j = 70;
+        int b = ++j;//First j is Increment and then use
+        int d = j++;
+        System.out.println(a);
+        System.out.println(c);
+        System.out.println(b);
+        System.out.println(d);
+    }
+}
