434887e37d8aa3195115f4652082c8ac6277b74b
Rahul Kumar <rahulkumar906960@gmail.com>
2026-06-04 21:36:27 +0530

    Add files via upload

---
+++ Java Langage/whileloop.java
@@ -0,0 +1,21 @@
+/*--------------While Loop---------------------------*/
+class whileloop{
+    public static void main(String[] args){
+        int i = 1;
+        while(i <= 7){//Syntax
+            System.out.println(i);
+            i++;
+        }
+    }
+}
+
+/*Q.WAP to print Natural numbers from 100 to 200.*/
+class whileloop{
+    public static void main(String[] args){
+        int i = 100;
+        while(i <= 200){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
