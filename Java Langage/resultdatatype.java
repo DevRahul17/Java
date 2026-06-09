9e3180fcb4b49cd08bbe655b9f0c86b054c801a1
Rahul Kumar <rahulkumar906960@gmail.com>
2026-06-09 20:14:29 +0530

    Update resultdatatype.java

--- Java Langage/resultdatatype.java
+++ Java Langage/resultdatatype.java
@@ -5,7 +5,7 @@ class resultdatatype{
         int b = 12;
         short c = 17;
         int d = a + b;
-        float e = 6.5f + c; 
+        float e = 6.5f + c;
         double f = d + e;
         System.out.println(d);
         System.out.println(e);
